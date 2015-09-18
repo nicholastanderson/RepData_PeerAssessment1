55540c20b9e8c1a8110f65ec79d5dd4813ac96aa
nicholastanderson <ntanderson10@gmail.com>
2015-09-18 00:03:50 -0500

    Deleting test.R

--- test.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-setwd("C:/Users/nicholas.anderson/Documents/R/ReproducibleResearch/RepData_PeerAssessment1")
-
-data<-read.csv("activity.csv",stringsAsFactors=FALSE)
-data[,"date"]<-as.Date(data[,"date"])
-
-data_date<-group_by(data,date)
-data_mean<-summarise(data_date,mean(steps,na.rm=TRUE))
-print(data_mean)
